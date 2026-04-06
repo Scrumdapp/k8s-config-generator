397b1ed0f348b12d43edcb7ad4931e7db864b64b
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-04-06 18:08:11 +0200

    fix echo missing property

--- src/command/commands/echoCommand.ts
+++ src/command/commands/echoCommand.ts
@@ -7,7 +7,7 @@ export const echoCommand: CommandType = {
         node.data.echo = getSchemaValue(node, schema, "echo");
     },
     run: (ctx, node) => {
-        const message = getStringValue(node, "echo")
+        const message = getStringValue(ctx, node, "echo")
         console.log(`echo: ${message}`)
     }
 }
